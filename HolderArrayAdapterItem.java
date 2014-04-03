059dc89b40e3ad6f48d7961875fdfe53bf4bd2b1
Aleksandar Micić <aleksandar.micic86@gmail.com>
2014-04-04 01:26:31 +0200

    Update HolderArrayAdapterItem.java
    Interface suits it better

--- HolderArrayAdapterItem.java
+++ HolderArrayAdapterItem.java
@@ -2,8 +2,8 @@ package com.sixteen11.ui;
 
 import android.view.View;
 
-public abstract class HolderArrayAdapterItem<T> {
-    abstract public T newViewHolder();
-    abstract public void initViewHolder(View view, Object holder);
-    abstract public void updateViewHolder(Object holder);
+public interface HolderArrayAdapterItem<T> {
+    public T newViewHolder();
+    public void initViewHolder(View view, Object holder);
+    public void updateViewHolder(Object holder);
 }
